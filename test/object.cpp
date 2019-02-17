3c0783869c929b88313b8868917c6dfacbf30406
Jeff Garzik <jeff@bloq.com>
2019-02-16 22:59:35 -0500

    test/object: add basic encode/decode unit tests

--- test/object.cpp
+++ test/object.cpp
@@ -4,6 +4,7 @@
 // file COPYING or https://opensource.org/licenses/mit-license.php.
 
 #include <stdint.h>
+#include <string.h>
 #include <vector>
 #include <string>
 #include <map>
@@ -109,6 +110,52 @@ BOOST_AUTO_TEST_CASE(rlpvalue_array)
 
 BOOST_AUTO_TEST_CASE(rlpvalue_readwrite)
 {
+    unsigned char s1[] = { 0x83, 'd', 'o', 'g' };
+
+    RLPValue v;
+    size_t consumed, wanted;
+    bool rc = v.read(&s1[0], sizeof(s1), consumed, wanted);
+    BOOST_CHECK(rc);
+    BOOST_CHECK(v.isBuffer());
+    BOOST_CHECK_EQUAL(v.getValStr(), "dog");
+    BOOST_CHECK_EQUAL(consumed, sizeof(s1));
+    BOOST_CHECK_EQUAL(wanted, 0);
+
+    std::string s1_out = v.write();
+    BOOST_CHECK_EQUAL(sizeof(s1), s1_out.size());
+    BOOST_CHECK_EQUAL(memcmp(&s1[0], &s1_out[0], sizeof(s1)), 0);
+
+    unsigned char s2[] =
+    { 0xc8, 0x83, 'c', 'a', 't', 0x83, 'd', 'o', 'g' };
+
+    RLPValue v2;
+    rc = v2.read(&s2[0], sizeof(s2), consumed, wanted);
+    BOOST_CHECK(rc);
+    BOOST_CHECK(v2.isArray());
+    BOOST_CHECK_EQUAL(consumed, sizeof(s2));
+    BOOST_CHECK_EQUAL(wanted, 0);
+    BOOST_CHECK_EQUAL(v2[0].getValStr(), "cat");
+    BOOST_CHECK_EQUAL(v2[1].getValStr(), "dog");
+
+    std::string s2_out = v2.write();
+    BOOST_CHECK_EQUAL(sizeof(s2), s2_out.size());
+    BOOST_CHECK_EQUAL(memcmp(&s2[0], &s2_out[0], sizeof(s2)), 0);
+
+    unsigned char s_empty_list[] = { 0xc0 };
+    RLPValue v_empty_list;
+    rc = v_empty_list.read(&s_empty_list[0], sizeof(s_empty_list), consumed, wanted);
+    BOOST_CHECK(rc);
+    BOOST_CHECK(v_empty_list.isArray());
+    BOOST_CHECK_EQUAL(v_empty_list.size(), 0);
+
+    unsigned char s_null[] = { 0x80 };
+    RLPValue v_null;
+    rc = v_null.read(&s_null[0], sizeof(s_null), consumed, wanted);
+    BOOST_CHECK(rc);
+    BOOST_CHECK(v_null.isBuffer());
+    BOOST_CHECK_EQUAL(v_null.getValStr(), "");
+    BOOST_CHECK_EQUAL(consumed, sizeof(s_null));
+    BOOST_CHECK_EQUAL(wanted, 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
